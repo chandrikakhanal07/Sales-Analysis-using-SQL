607f424081cdfbd0a5b5e0600e2f8221bf477653
Chandrika khanal <67287230+chandrikakhanal07@users.noreply.github.com>
2023-05-08 10:21:27 -0400

    Update sales analysis.sql

--- sales analysis.sql
+++ sales analysis.sql
@@ -66,28 +66,91 @@ order by 3 DESC;
 
 ## RFM analysis- indexing technique  
 ## who's best customer 
+
+
+
+DROP TABLE IF EXISTS #rfm 
 with rfm as 
 (
+select 
+		CUSTOMERNAME, 
+		sum(sales) MonetaryValue,
+		avg(sales) AvgMonetaryValue,
+		count(ORDERNUMBER) Frequency,
+		max(ORDERDATE) last_order_date,
+		(select max(ORDERDATE) from [dbo].[sales_data_sample]) max_order_date,
+		DATEDIFF(DD, max(ORDERDATE), (select max(ORDERDATE) from [dbo].[sales_data_sample])) Recency
+	from [PortfolioDB].[dbo].[sales_data_sample]
+	group by CUSTOMERNAME
+),
+rfm_calc as
+(
 
-select customername,
-sum(Sales) as revenue,
-avg(sales) as avgmonetaryvalue,
-count(ordernumber) frequency,
-max(orderdate) as last_order_date,
-(select max(ORDERDATE) from  portfolio.`sales-data-sample` ) as max_order_date,
-datediff(DD,max(orderdate),(select(orderdate) from  portfolio.`sales-data-sample` )) as recency
-from  portfolio.`sales-data-sample` 
-group by customername
-), 
-rfm_calc as (
-select r.*,
-ntile(4) over (order by recency) rfm_recency ,
-ntile(4) over (order by frequency) rfm_frequency ,
-ntile(4) over (order by avgmonetaryvalue) rfm_monetary 
-from rfm as r 
-order by  4 desc
-) 
-select c.* 
+	select r.*,
+		NTILE(4) OVER (order by Recency desc) rfm_recency,
+		NTILE(4) OVER (order by Frequency) rfm_frequency,
+		NTILE(4) OVER (order by MonetaryValue) rfm_monetary
+	from rfm r
+)
+select 
+	c.*, rfm_recency+ rfm_frequency+ rfm_monetary as rfm_cell,
+	cast(rfm_recency as varchar) + cast(rfm_frequency as varchar) + cast(rfm_monetary  as varchar)rfm_cell_string
+into #rfm
 from rfm_calc c
 
+select CUSTOMERNAME , rfm_recency, rfm_frequency, rfm_monetary,
+	case 
+		when rfm_cell_string in (111, 112 , 121, 122, 123, 132, 211, 212, 114, 141) then 'lost_customers'  --lost customers
+		when rfm_cell_string in (133, 134, 143, 244, 334, 343, 344, 144) then 'slipping away, cannot lose' -- (Big spenders who haven’t purchased lately) slipping away
+		when rfm_cell_string in (311, 411, 331) then 'new customers'
+		when rfm_cell_string in (222, 223, 233, 322) then 'potential churners'
+		when rfm_cell_string in (323, 333,321, 422, 332, 432) then 'active' --(Customers who buy often & recently, but at low price points)
+		when rfm_cell_string in (433, 434, 443, 444) then 'loyal'
+	end rfm_segment
 
+from #rfm
+
+
+
+--What products are most often sold together? 
+--select * from [dbo].[sales_data_sample] where ORDERNUMBER =  10411
+
+select distinct OrderNumber, stuff(
+
+	(select ',' + PRODUCTCODE
+	from [dbo].[sales_data_sample] p
+	where ORDERNUMBER in 
+		(
+
+			select ORDERNUMBER
+			from (
+				select ORDERNUMBER, count(*) rn
+				FROM [PortfolioDB].[dbo].[sales_data_sample]
+				where STATUS = 'Shipped'
+				group by ORDERNUMBER
+			)m
+			where rn = 3
+		)
+		and p.ORDERNUMBER = s.ORDERNUMBER
+		for xml path (''))
+
+		, 1, 1, '') ProductCodes
+
+from [dbo].[sales_data_sample] s
+order by 2 desc
+
+--Which city has the highest number of sales in a specific country
+select city, sum (sales) Revenue
+from [PortfolioDB].[dbo].[sales_data_sample]
+where country = 'UK'
+group by city
+order by 2 desc
+
+
+
+---Which is the best product in United States?
+select country, YEAR_ID, PRODUCTLINE, sum(sales) Revenue
+from [PortfolioDB].[dbo].[sales_data_sample]
+where country = 'USA'
+group by  country, YEAR_ID, PRODUCTLINE
+order by 4 desc
